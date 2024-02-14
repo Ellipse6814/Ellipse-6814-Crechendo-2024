3270e8d2913228db5a5dccc72e54362b7bf97c0f
JV1ST <78391100+JV1ST@users.noreply.github.com>
2024-02-14 15:12:36 -0800

    surgically inserted swerve

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,119 @@
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix6.*;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfigurator;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
+
+public class SwerveModule {
+
+
+    private final TalonFX driveMotor;
+    private final TalonFX turningMotor;
+
+    private final PIDController turningPidController;
+
+    
+    private final CANcoder absoluteEncoder;
+    private final boolean absoluteEncoderReversed;
+    private final double absoluteEncoderOffsetRad;
+
+    public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+            int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+
+        this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
+        absoluteEncoder = new CANcoder(absoluteEncoderId);
+        driveMotor = new TalonFX(driveMotorId);
+        turningMotor = new TalonFX(turningMotorId);
+        
+        driveMotor.setInverted(driveMotorReversed);
+        turningMotor.setInverted(turningMotorReversed);
+
+
+
+        turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+        
+        
+        
+        resetEncoders();
+        
+    }
+
+    public double getDrivePosition() {
+       
+        return driveMotor.getPosition().getValue() * ModuleConstants.kDriveEncoderRot2Meter;
+        
+    }
+
+    public double getTurningPosition() {
+        return turningMotor.getPosition().getValue() * ModuleConstants.kTurningEncoderRot2Rad;
+    }
+
+    public double getDriveVelocity() {
+        return driveMotor.getVelocity().getValue() * ModuleConstants.kDriveEncoderRPM2MeterPerSec;
+    }
+
+    public double getTurningVelocity() {
+        return turningMotor.getVelocity().getValue() * ModuleConstants.kTurningEncoderRPM2RadPerSec;
+    }
+
+    public double getAbsoluteEncoderRad() {
+        double angle = absoluteEncoder.getAbsolutePosition().getValue();
+        angle *= (Math.PI * 2);
+        angle -= absoluteEncoderOffsetRad;
+        return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+
+    }
+
+    public void resetEncoders() {   
+        driveMotor.setPosition(0);
+        turningMotor.setPosition(getAbsoluteEncoderRad()/Constants.ModuleConstants.kTurningEncoderRot2Rad);
+    }
+    
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public SwerveModulePosition getPosition(){
+        return new SwerveModulePosition(getDrivePosition(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+        state = SwerveModuleState.optimize(state, getState().angle);
+        driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getDeviceID() + "] state", state.toString());
+    }
+
+    public void writeValues(){  
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getDeviceID() + "] state", 
+        getState().toString() + " abs encoder rad " + getAbsoluteEncoderRad());
+
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getDeviceID() + "] drive pos", getPosition().toString());
+    }
+
+    public void stop() {
+        driveMotor.set(0);
+        turningMotor.set(0);
+    }
+}
