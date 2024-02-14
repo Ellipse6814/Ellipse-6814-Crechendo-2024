cda6141fdfb72403d8af51c18cd3dd4e371d54c8
stupidjuice <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2024-02-14 14:51:26 -0800

    Climb subsystem

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -4,33 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimbConstants;
 
 public class ClimbSubsystem extends SubsystemBase {
+  public CANSparkMax leftMotor = new CANSparkMax(ClimbConstants.kLeftMotorPort, MotorType.kBrushless);
+  public CANSparkMax rightMotor = new CANSparkMax(ClimbConstants.kRightMotorPort, MotorType.kBrushless);
+  public RelativeEncoder leftEncoder = leftMotor.getEncoder();
+  public RelativeEncoder rightEncoder = rightMotor.getEncoder();
+
   /** Creates a new ExampleSubsystem. */
   public ClimbSubsystem() {
 
-
-
   }
 
-
   public void setRightMotor(double speed){
-
+    rightMotor.set(speed);
   }
 
   public void setLeftMotor(double speed){
-
-
+    leftMotor.set(speed);
   }
   
   public double getLeftEncoder(){
-    return 0.0;
+    return leftEncoder.getPosition();
   }
 
   public double getRightEncoder(){
-    return 0.0;
+    return rightEncoder.getPosition();
 
   }
 
