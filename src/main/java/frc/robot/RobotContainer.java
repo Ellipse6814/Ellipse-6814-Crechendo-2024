d859f9858ba9d2c1dcd0c7caa006c0ee09c99b54
Blooncineration <155571135+JackDriscoll1@users.noreply.github.com>
2024-03-11 16:56:23 -0700

    State of the code after SF Regionals

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.ArmRaiseCommand;
 import frc.robot.commands.ShooterBackWheelCommand;
@@ -12,15 +14,32 @@ import frc.robot.commands.ShooterIntakeBottomRollerCommand;
 import frc.robot.commands.ShooterIntakeCommand;
 import frc.robot.commands.ShooterSourceCommand;
 import frc.robot.commands.ShooterSpeakerAmpTrapCommand;
+import frc.robot.commands.SwerveJoystickCmd;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.ClimbCommandJoystick;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -38,27 +57,41 @@ public class RobotContainer {
   public final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
   private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
   public final ArmSubsystem m_armSubsystem = new ArmSubsystem(); 
-  private final IntakeSubsystem m_exampleSubsystem = new IntakeSubsystem();
+  public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
-  private final ClimbSubsystem m_ClimbSubsystem = new ClimbSubsystem();
 
   private final Joystick driverJoytick = new Joystick(OIConstants.kDriverControllerPort);
 
-  private final Joystick joystick = new Joystick(0);
+  private final Joystick joystick = new Joystick(2);
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    m_ClimbSubsystem.setDefaultCommand(new ClimbCommandJoystick(m_ClimbSubsystem, () -> driverJoytick.getRawAxis(2) * 0.3, () -> driverJoytick.getRawAxis(4) * 0.3));
+    
+      swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
+                swerveSubsystem,
+                () -> -driverJoytick.getRawAxis(OIConstants.kDriverYAxis),
+                () -> driverJoytick.getRawAxis(OIConstants.kDriverXAxis),
+                () -> driverJoytick.getRawAxis(OIConstants.kDriverRotAxis),
+                () -> !driverJoytick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx),
+                m_armSubsystem));
+
+    
+    
     configureBindings();
+    // Configure the trigger bindings
   }
 
 
-  private void configureBindings() {
-    // new JoystickButton(joystick, 1).onTrue(new ShooterSourceCommand(m_shooterSubsystem, m_intakeSubsystem));
+  private void configureBindings() { 
+
+    new JoystickButton(joystick, 5).onTrue(new ArmRaiseCommand(m_armSubsystem, Math.toRadians(-1)).withTimeout(2.5));
+    new JoystickButton(joystick, 6).onTrue(new ArmRaiseCommand(m_armSubsystem, Math.toRadians(85)));
+    //new JoystickButton(joystick, 1).onTrue(new ShooterSourceCommand(m_shooterSubsystem, m_intakeSubsystem));
     // new JoystickButton(joystick, 2).onTrue(new ShooterBackWheelCommand(m_shooterSubsystem).raceWith(new WaitCommand(1.0)).andThen(new ShooterSpeakerAmpTrapCommand(m_shooterSubsystem, m_intakeSubsystem)));
-    new JoystickButton(joystick, 3).onTrue(new ArmRaiseCommand(m_armSubsystem, Math.toRadians(60)));
-    //new JoystickButton(joystick, 4).onTrue(new ShooterIntakeCommand(m_shooterSubsystem, m_intakeSubsystem));
+    
+    new JoystickButton(joystick, 4).onTrue(new ShooterIntakeCommand(m_shooterSubsystem, m_intakeSubsystem));
+    new JoystickButton(joystick, 1).onTrue(new ShooterSpeakerAmpTrapCommand(m_shooterSubsystem, m_intakeSubsystem));
+   
   }
 
   /**
@@ -68,6 +101,49 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-   return null;
+     // 1. Create trajectory settings
+        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+                AutoConstants.kMaxSpeedMetersPerSecond,
+                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                        .setKinematics(DriveConstants.kDriveKinematics);
+
+        // 2. Generate trajectory
+        //pos x = forward
+        //neg x = back
+        //pos y = right
+        //neg y = left
+        //This is all you should need for the robot container implementation of the limelight stuff: Pose2d(limelightSubsystem.getBotposeTableEntry(0), limelightSubsystem.getBotposeTableEntry(1), limelightBotposeTableEntry(5))
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(
+                new Translation2d(2, 0)
+                ),
+        new Pose2d(4, 0, Rotation2d.fromDegrees(0)),
+        trajectoryConfig);
+
+        // 3. Define PID controllers for tracking trajectory
+        PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+        PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+        ProfiledPIDController thetaController = new ProfiledPIDController(
+                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        // 4. Construct command to follow trajectory
+        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+                trajectory,
+                swerveSubsystem::getPose,
+                DriveConstants.kDriveKinematics,
+                xController,
+                yController,
+                thetaController,
+                swerveSubsystem::setModuleStates,
+                swerveSubsystem);
+
+        // 5. Add some init and wrap-up, and return everything
+        return new SequentialCommandGroup(
+                new WaitCommand(8),
+                new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
+                swerveControllerCommand,
+                new InstantCommand(() -> swerveSubsystem.stopModules()));
   }
 }
--- src/main/java/frc/robot/commands/ShooterIntakeCommand.java
+++ src/main/java/frc/robot/commands/ShooterIntakeCommand.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -33,10 +34,12 @@ public class ShooterIntakeCommand extends Command {
 
   @Override
   public void execute() {
-    m_shooterSubsystem.setMotor1(Constants.ShooterConstants.kShooterIntakeSpeed);
-    m_shooterSubsystem.setMotor2(0);
-    m_shooterSubsystem.setMotor3(0);
-    m_intake.setspeed(-1 * Constants.ShooterConstants.kShooterIntakeSpeed);
+      SmartDashboard.putString("is it running?", "yes");
+      m_shooterSubsystem.setMotor1(Constants.ShooterConstants.kShooterIntakeSpeed - 0.2);
+      m_shooterSubsystem.setMotor2(0);
+      m_shooterSubsystem.setMotor3(0);
+      m_intake.setspeed(-1 * Constants.ShooterConstants.kShooterIntakeSpeed);
+    
 
 
   }
@@ -45,11 +48,12 @@ public class ShooterIntakeCommand extends Command {
   public void end(boolean interrupted) {
     m_shooterSubsystem.stop();
     m_intake.stop();
+    
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_shooterSubsystem.getSensor1;
+    return m_shooterSubsystem.getSensor1();
   }
 }
