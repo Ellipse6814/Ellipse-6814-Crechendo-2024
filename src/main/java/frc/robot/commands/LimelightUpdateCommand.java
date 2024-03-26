b9c9f3b563bc6d13333704bec930611263634a92
Blooncineration <163072292+TeamEllipse6814@users.noreply.github.com>
2024-03-26 11:38:25 -0700

    Fixed wrong number in Limlight Update Command

--- src/main/java/frc/robot/commands/LimelightUpdateCommand.java
+++ src/main/java/frc/robot/commands/LimelightUpdateCommand.java
@@ -23,7 +23,7 @@ public class LimelightUpdateCommand extends Command {
 
     @Override
     public void execute() {
-        Rotation2d rotation = new Rotation2d(Math.toRadians(m_Limelight.getBotPoseTableEntry(0)));
+        Rotation2d rotation = new Rotation2d(Math.toRadians(m_Limelight.getBotPoseTableEntry(5)));
         Pose2d pose = new Pose2d(m_Limelight.getBotPoseTableEntry(0), m_Limelight.getBotPoseTableEntry(1), rotation);
 
         if (m_Limelight.getBotPoseTableEntry(7) == 2) {
